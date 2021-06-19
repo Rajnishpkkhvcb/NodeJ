648ede29b2c3e6ac39be4222236571f7277f7f0f
Rajnish <rajnishpal161@gmail.com>
2021-06-19 20:06:17 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const adminRoutes=require('./routes/admin.js')
+const shopRoutes=require('./routes/shop.js')
+const app = express();
+app.use('/admin',adminRoutes);
+
+app.use(shopRoutes)
+
+
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+app.listen(3000);
